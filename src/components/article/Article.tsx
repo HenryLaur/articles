3636161450c73a33d48a081c202ea91674a9a8d2
Henry Laur <hlaur1344@gmail.com>
2020-08-13 17:11:27 +0300

    changed article style

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -5,6 +5,7 @@ import {
   CardMedia,
   CardContent,
   Typography,
+  Grid,
 } from "@material-ui/core";
 import { Article as IArticle } from "./ArticleSlice";
 import { ArticleModal } from "../modal/ArticleModal";
@@ -23,27 +24,24 @@ export const Article: React.FC<IArticle> = ({
   return (
     <div className="card">
       <Card onClick={() => setModalOpen(true)}>
-        <CardActionArea>
-          <CardMedia component="img" height="140" src={pictureUrl} />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              {title}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              {contentSnippet}
-            </Typography>
-          </CardContent>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {author}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {date.getUTCDate() +
-              "/" +
-              (date.getUTCMonth() + 1) +
-              "/" +
-              date.getUTCFullYear()}
-          </Typography>
-        </CardActionArea>
+        <CardMedia component="img" height="140" src={pictureUrl} />
+        <Typography gutterBottom variant="h5" component="h2">
+          {title}
+        </Typography>
+        <Typography className="author" variant="body2" component="p">
+          {author && "Author: " + author}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {contentSnippet}
+        </Typography>
+
+        <Typography variant="body2" color="textSecondary" component="p">
+          {date.getUTCDate() +
+            "/" +
+            (date.getUTCMonth() + 1) +
+            "/" +
+            date.getUTCFullYear()}
+        </Typography>
       </Card>
       <ArticleModal
         handleClose={() => setModalOpen(false)}
